6d20eeb1a95f6435618796329a22b87fe40edd54
Oleksandr Vynogradov <o.vynogradov@epages.com>
2017-10-16 12:36:00 +0200

    Remove wrong dependency

--- src/main/java/com/epages/phraseapp/PhraseAppProjectIdProvider.java
+++ src/main/java/com/epages/phraseapp/PhraseAppProjectIdProvider.java
@@ -1,9 +1,7 @@
 package com.epages.phraseapp;
 
-import com.sun.istack.internal.NotNull;
-
 public interface PhraseAppProjectIdProvider {
 
-    @NotNull String getProjectId();
+    String getProjectId();
 
 }
